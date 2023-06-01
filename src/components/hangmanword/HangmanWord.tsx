330ca4c25b92d4daed018026f93a17ce7c34027c
nicod12 <dumenicolas45@gmail.com>
2023-06-01 09:34:17 -0300

    upd1

--- src/components/hangmanword/HangmanWord.tsx
+++ src/components/hangmanword/HangmanWord.tsx
@@ -1,5 +1,18 @@
 export function HangmanWord() {
+    const word = "test";
     return(
-        <></>
+        <section style={{
+                    display: "flex", 
+                    gap: ".25em", 
+                    fontSize: "6rem", 
+                    fontWeight: "bold",
+                    textTransform: "uppercase", 
+                    fontFamily: "monospace"
+                 }}
+        >
+            {word.split("").map((letter, index) => (
+                <span style={{borderBottom: ".1em solid black"}}>{letter}</span>
+            ))}            
+        </section>
     )
 }
